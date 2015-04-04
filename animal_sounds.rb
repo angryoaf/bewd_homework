e11d482b9648fef30f5c3e28c685fd9f93d9c374
Laury Woollery <laury@angryoaf.com>
2015-04-04 10:45:47 -0700

    Stupid fat fingered typo.

--- animal_sounds.rb
+++ animal_sounds.rb
@@ -3,7 +3,7 @@ puts "What's an animal that makes a sound?"
 ap1_animal = gets.chomp.gsub(/[^a-zA-Z0-9_]/i, "")
 
 puts "what sound does a " + ap1_animal + " make?"
-z
+
 ap1_animal_sound = gets.chomp.gsub(/[^a-zA-Z0-9_]/i, "") 
 
 puts "How many times does the " + ap1_animal + " go " + ap1_animal_sound +"?"
