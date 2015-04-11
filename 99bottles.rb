4a8c23d39764ddea32886296d1693cb05915e764
Laury Woollery <laury@angryoaf.com>
2015-04-10 19:35:47 -0700

    add 2 homework files. 99bottles & grandma.rb

---
+++ 99bottles.rb
@@ -0,0 +1,8 @@
+bottles = 99
+
+while bottles > 1
+	puts "#{bottles} bottles of beer on the wall, #{bottles}  bottles of beer.\n Take one down and pass it around, #{bottles = bottles -1} bottles of beer on the wall.\n\n "
+end
+		puts "#{bottles} bottle of beer on the wall #{bottles} bottle of beer"
+		puts "Take it down and pass it around, no more bottles of beer on the wall."
+
---
+++ grandma.rb
@@ -0,0 +1,42 @@
+seed_1 = true
+puts "Grandma: HI SONNY! SO NICE OF YOU TO VISIT GRANDMA!"
+puts "Be polite! Say something to your grandma."
+
+
+
+while seed_1
+	something = gets.chomp
+if something == "BYE"
+	bye_seed = bye_see + 1
+	seed_1 = false && bye_see = 3
+
+
+elsif something.downcase == "something to your grandma" || something.downcase == "something to your grandma."
+	puts "
+IT SEEMS WE HAVE A JOKESTER ON OUR HANDS! LOLZ
+	  ▄▄▄▄▀▀▀▀▀▀▀▀▄▄▄▄▄▄
+    █    ▒▒▒▒▒▒▒▒▒▒▒▒  ▀▀▄
+   █   ▒▒▒▒▒▒        ▒▒▒  █
+  █      ▄██▀▄▄     ▄▄▄   █
+ ▀▒▄▄▄▒ █▀▀▀▀▄▄█   ██▄▄█   █
+█▒█▒▄ ▀▄▄▄▀        █   ▒▒▒▒▒█
+█▒█ █▀▄▄     █▀    ▀▄  ▄▀▀▀▄▒█
+ █▀▄ █▄ █▀▄▄ ▀ ▀▀ ▄▄▀    █  █
+  █  ▀▄▀█▄▄ █▀▀▀▄▄▄▄▀▀█▀██ █
+   █  ██  ▀█▄▄▄█▄▄█▄████ █
+    █   ▀▀▄ █   █ ███████ █
+     ▀▄   ▀▀▄▄▄█▄█▄█▄█▄▀  █
+       ▀▄▄ ▒▒▒▒          █
+          ▀▀▄▄ ▒▒▒▒▒▒▒▒▒▒ █
+              ▀▄▄▄▄▄     █"
+
+
+elsif something == something.upcase
+	puts "Grandma: \"NO, NOT SINCE #{rand(1930..1950)}\""
+
+else 
+	puts "Gandma: \"HUH?! SPEAK UP, SONNY!\""
+	end
+end
+
+puts "Grandma: \"THANKS FOR VISITING, SONNY!\"\n\"NOW GIVE GRANDMA A FOOT RUB BEFORE YOU LEAVE\"."
