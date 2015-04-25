ca48a123a2be4e34dd50c9052dcd5771a68eccb5
Laury Woollery <laury@angryoaf.com>
2015-04-25 08:10:42 -0700

    Update ruby program to display only first 20 fiction

--- class-08/usa-booklist.rb
+++ class-08/usa-booklist.rb
@@ -2,24 +2,39 @@ require 'httparty'
 
 
 #usatoday_books ='http://mashable.com/stories.json'
-usatoday_books ='http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key='
+usatoday_books ='http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key=
 
 usa_api_response = HTTParty.get(usatoday_books)
 usatd_parser = usa_api_response.parsed_response
 
 counter = 0
+seed = 0
+
 booklist_20 = (usatd_parser['BookLists'][1]['BookListEntries'])
 
- 20.times do
-	puts "#{counter+1}: #{booklist_20[counter]['Title']}"
-	puts "Author: #{booklist_20[counter]['Author']}"
-		if 
-			booklist_20[counter]['BriefDescription'] == ''
-			puts ''
-		else
-			puts "Description: #{booklist_20[counter]['BriefDescription']}"
-		end
+	booklist_20.each do |list_hash|	
 
-	puts "\n------------------------\n\n"
-	counter = counter+1
-end
+			if (booklist_20[counter]['Category']['CategoryID']).to_i == 151 && seed < 20
+
+				seed = seed +1
+
+
+				puts "booknumber: #{seed}"
+
+				puts booklist_20[counter]['Category']['CategoryName']
+					puts "#{counter+1}: #{booklist_20[counter]['Title']}"
+					puts "Author: #{booklist_20[counter]['Author']}"
+						if 
+							booklist_20[counter]['BriefDescription'] == ''
+							puts ''
+						else
+							puts "Description: #{booklist_20[counter]['BriefDescription']}"
+						end
+
+					puts "\n------------------------\n\n"
+				
+				counter = counter + 1
+			else
+				counter = counter +1
+			end
+	end
