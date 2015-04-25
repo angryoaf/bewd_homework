c2e9617df8207844c0acbfe19ad24a0bae52a92e
Laury Woollery <laury@angryoaf.com>
2015-04-24 23:27:41 -0700

    api key

--- class-08/usa-booklist.rb
+++ class-08/usa-booklist.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 
 #usatoday_books ='http://mashable.com/stories.json'
-usatoday_books ='http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key=7maw2qgv8xgbcw5c7nfcm6qs'
+usatoday_books ='http://api.usatoday.com/open/bestsellers/books/ThisWeek?api_key='
 
 usa_api_response = HTTParty.get(usatoday_books)
 usatd_parser = usa_api_response.parsed_response
